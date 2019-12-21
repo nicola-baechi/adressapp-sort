a515618c34f4ca758b59cc921a363d9de306e4da
nicolabaechigithub <nicola.baechi@lernende.bbw.ch>
2019-12-21 14:52:21 +0100

    versucht Tabellenanzeige zu machen

--- src/main/java/ch/schule/AddressApp/AdressModel.java
+++ src/main/java/ch/schule/AddressApp/AdressModel.java
@@ -1,6 +1,7 @@
 package ch.schule.AddressApp;
 
 import java.util.ArrayList;
+import javafx.collections.ObservableList;
 
 /**
  * AdressModel
@@ -8,9 +9,27 @@ import java.util.ArrayList;
 public class AdressModel {
 
     private ArrayList<Person> plist;
+    private boolean areContacts;
 
     public AdressModel(){
         plist = new ArrayList<>();
+        areContacts = false;
+    }
+
+    public ArrayList<Person> getPlist() {
+        return plist;
+    }
+
+    public void setPlist(ArrayList<Person> plist) {
+        this.plist = plist;
+    }
+
+    public boolean getAreContacts() {
+        return areContacts;
+    }
+
+    public void setAreContacts(boolean areContacts) {
+        this.areContacts = areContacts;
     }
 
     public void addPerson(Person p){
@@ -26,7 +45,7 @@ public class AdressModel {
     public void printAllPersons(){
         int counter = 1;
         for(Person p: plist){
-            System.out.println(counter + "> " + "Vorname: " + p.getFirstname() + " | " + "Nachname: " + p.getLastname());
+            System.out.println(counter + " > " + "Vorname: " + p.getFirstname() + " | " + "Nachname: " + p.getLastname());
             System.out.println("Strasse: " + p.getStreet());
             System.out.println("Stadt: " + p.getCity());
             System.out.println("Postleitzahl: " + p.getPostalcode());
@@ -35,4 +54,7 @@ public class AdressModel {
             counter++;
         }
     }
+
+    
+
 }
--- src/main/java/ch/schule/AddressApp/Controller.java
+++ src/main/java/ch/schule/AddressApp/Controller.java
@@ -2,12 +2,14 @@ package ch.schule.AddressApp;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import javafx.scene.control.TableColumn;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +19,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  * Controller
@@ -25,17 +31,26 @@ public class Controller {
 
     @FXML
     Label firstname, lastname, street, city, postalcode, birthday;
-    
+
     @FXML
     TextField nfirstname, nlastname, nstreet, ncity, npostalcode, nbirthday; // textfields of newPerson.fxml
 
+    // configuring the table
+    @FXML 
+    private TableView<Person> table;
+    @FXML 
+    private TableColumn<Person, String> fnameColumn;
+    @FXML 
+    private TableColumn<Person, String> lnameColumn;
+
+
     AdressModel model = new AdressModel();
 
-    public void handleButtonDelete(ActionEvent event){
+    public void handleButtonDelete(ActionEvent event) {
         System.out.println("Test");
     }
 
-    public void handleButtonNew(ActionEvent event) throws Exception{
+    public void handleButtonNew(ActionEvent event) throws Exception {
         System.out.println("creating new contact");
         URL url = new File("src/main/resources/fxml/NewPerson.fxml").toURI().toURL();
         Parent newParent = FXMLLoader.load(url);
@@ -45,21 +60,45 @@ public class Controller {
         newStage.show();
     }
 
-    public void handleButtonAddNew(ActionEvent event){
+    public void handleButtonAddNew(ActionEvent event) throws IOException {
         System.out.println("new contact added");
         Person person = new Person(nfirstname.getText(), nlastname.getText(), nstreet.getText(), ncity.getText(), npostalcode.getText(), nbirthday.getText());
         model.addPerson(person);
         model.printAllPersons();
+        model.setAreContacts(true);
+        URL url = new File("src/main/resources/fxml/PersonOverview.fxml").toURI().toURL();
+        Parent overviewParent = FXMLLoader.load(url);
+        Scene personOverview = new Scene(overviewParent);
+        Stage stageOverview = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stageOverview.setScene(personOverview);
+        stageOverview.show();
+        System.out.println(model.getAreContacts());
     }
 
         
 
     public void handleButtonEdit(ActionEvent event){
-        //TODO
+        
+    }
+
+    public ObservableList<Person> getListofPersons(){
+        ObservableList<Person> oblist = FXCollections.observableArrayList();
+        oblist.add(new Person("a","b","c","d","e", "f"));
+        oblist.add(new Person("a","b","c","d","e", "f"));
+            //for(Person p: oblist){
+            //oblist.add(new Person(p.getFirstname(), p.getLastname()));
+            //System.out.println(p.getFirstname() + " " + p.getLastname());
+        //}  
+        return oblist;
     }
 
     @FXML
     private void initialize(){
-        
+        // setting up columns in the table
+        //if(model.getAreContacts()){ 
+        fnameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("firstname"));
+        lnameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("lastname"));
+        table.getItems().addAll(getListofPersons());
+        //}
     }
 }
